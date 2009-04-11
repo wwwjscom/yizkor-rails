bc97735500a768d5add6e6210de7104c45a344ed
Jason Soo <wwwjscom@gmail.com>
2009-04-10 20:46:23 -0500

    modularized code

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,11 +4,12 @@ class SearchesController < ApplicationController
   end
 
   def search
-    book = search_query(params[:find_by])
+    find_by = params[:find_by]
+    book = find_query(find_by)
     unless params[:commit].blank?
       if book.blank?
         flash.now[:notice] = 'Book not found'
-        render :action => 'search_by_title'
+        render :action => "find_by_#{find_by}"
       else
         redirect_to :controller => 'books', :action => 'show', :id => book.id
       end
@@ -17,7 +18,7 @@ class SearchesController < ApplicationController
 
   private #------------
 
-  def search_query(find_by)
+  def find_query(find_by)
     case find_by
       when 'title': Book.find_by_title(params[:title])
     end
