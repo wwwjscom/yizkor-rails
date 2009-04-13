0df0d9b1851a6eb6899392fc4e69b4fe0380a6ec
Jason Soo <wwwjscom@gmail.com>
2009-04-13 00:14:11 -0500

    link

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -24,34 +24,34 @@ class SearchesController < ApplicationController
   def find_query(find_by)
     case find_by
       when 'title': books = Book.find_by_title(params[:title])
+
       when 'contributor': 
         books = Contributor.find(:all, 
-          :conditions => 
-            ["first = ? AND last = ? AND role = ?", 
-            params[:first], 
-            params[:last], 
-            params[:Role]
-          ])
+                                 :conditions => 
+                                ["first = ? AND last = ? AND role = ?", 
+                                  params[:first], 
+                                  params[:last], 
+                                  params[:Role]
+                                ])
         # bug here - we only return the first otherwise we'll need
         # to loop over all the results returned, which I didn't
         # write here.
         books = Book.find(:first, :conditions => ["id IN (?)", books.collect { |b| b.id }])
+
       when 'advanced':
         conditions = ""
         # build conditions according to info given
         params_list = ['historical_period', 'call_num', 'abstract', 'keywords', 'other_notes', 'table_of_contents', 'has_toc', 'has_index', 'has_necrology']
         params_list.each do |param|
           unless params[param].blank?
-            logger.info "#{param} is not blank.  Its set to #{params[param]}"
             if conditions != ""
               conditions += " AND"
             end
             conditions += " #{param} = #{params[param]}"
           end
         end
-logger.info "Query: #{conditions}"
-books = Book.find(:first, :conditions => ["#{conditions}"])
-    end
+        books = Book.find(:first, :conditions => ["#{conditions}"])
+      end
     return books
   end
 end
