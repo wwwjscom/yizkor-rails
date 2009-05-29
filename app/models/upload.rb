86266be8e1b7888f16e7f450f781bbe38506ede4
Jason Soo <wwwjscom@gmail.com>
2009-05-29 02:51:25 -0500

    increased upload resolution

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,7 +6,7 @@ class Upload < ActiveRecord::Base
   has_attachment :content_type => :image, 
                  :storage => :file_system, 
                  :max_size => 500.kilobytes,
-                 :resize_to => '320x200>',
+                 :resize_to => '520x400>',
                  :thumbnails => { :thumb => '100x100>' },
                  :processor => 'ImageScience'
 
