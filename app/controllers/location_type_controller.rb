8b4a5969af05e71f062ac248a3ba0c1df668b2a7
Jason Soo <wwwjscom@gmail.com>
2009-10-29 16:14:33 -0500

    Moved some methods around

--- app/controllers/location_type_controller.rb
+++ app/controllers/location_type_controller.rb
@@ -1,14 +1,6 @@
 class LocationTypeController < ApplicationController
   def index
-    location_types = LocationType.find(:all)
-    alternate_locations = LocationVariation.something
-
-    # Merge the two arrays so that location variations
-    # appear within the list of locations
-    @merged = alternate_locations | location_types
-    
-    # Now sort them based on name
-    @merged.sort! { |x,y| x.name <=> y.name }
+    @merged = Location.all_locations
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/location.rb
+++ app/models/location.rb
@@ -6,4 +6,18 @@ class Location < ActiveRecord::Base
   def get_hash(loc_type_id)
     LocationType.find(loc_type_id)
   end
+
+  # This method returns all locations,
+  # mainly LocationType and LocationVariation
+  def self.all_locations
+    location_types = LocationType.find(:all)
+    alternate_locations = LocationVariation.something
+
+    # Merge the two arrays so that location variations
+    # appear within the list of locations
+    merged = alternate_locations | location_types                                                
+
+    # Now sort them based on name
+    merged.sort! { |x,y| x.name <=> y.name }
+  end
 end
