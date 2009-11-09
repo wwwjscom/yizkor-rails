a1e6d1bab234509ffaab2302ccff97ec7798e28f
Jason Soo <wwwjscom@gmail.com>
2009-11-09 10:49:07 -0600

    removed the USHMM header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def locations_name(l)
+    if not l.location_type_id.blank?
+      l.location_type.name
+    else
+      l.location_variation.name
+    end
+  end
 end
