cda9743f051278f73d395b42210e8b46bae3c9ed
Jason Soo <wwwjscom@gmail.com>
2010-06-15 13:50:05 -0500

    PostgreSQL fix

--- db/migrate/20090323015504_create_books.rb
+++ db/migrate/20090323015504_create_books.rb
@@ -1,6 +1,8 @@
 class CreateBooks < ActiveRecord::Migration
   def self.up
-    create_table :books, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
+    # The following line doesn't work on PostgreSQL
+    #create_table :books, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
+    create_table :books do |t|
       t.string :title
       t.integer :publication_year
       t.string :publication_place
